24ada7ac8298d8a2f56523441e20683b535b1ceb
Guillaume Giguère <2185208@cmontmorency.qc.ca>
2023-03-08 17:49:46 -0500

    Modification affichage

--- src/cours/components/Cours.js
+++ src/cours/components/Cours.js
@@ -11,7 +11,7 @@ function Cours({ cours }) {
         <Card className="user-item__content">
           <Link to={`/cours/${cours.id}`}>
             <div className="user-item__info">
-              <h2>{cours.titre + " + " + cours.discipline}</h2>
+              <h2>{cours.titre + " - " + cours.discipline}</h2>
             </div>
           </Link>
         </Card>
